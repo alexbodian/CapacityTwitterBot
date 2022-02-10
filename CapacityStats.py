f376422a4adbe6fc81cf7de772c4e1124762ab0c
Alex <polarzombies@gmail.com>
2022-02-10 15:30:27 -0500

    able to scrape currently staked amounts and circ supply

---
+++ CapacityStats.py
@@ -0,0 +1,80 @@
+import os
+import tweepyimport Tkinter
+import re
+import time
+import asyncio
+import time
+import pyppeteer
+from pyppeteer import launch
+
+
+async def main():
+    browser = await launch()
+
+    # # Gemini Pool
+    page = await browser.newPage()
+    await page.goto('https://app.flexa.network/explore/app/gemini')
+    time.sleep(7)
+    element = await page.querySelector(".css-i3vmyo")
+    title = await page.evaluate('(element) => element.textContent', element)
+    geminiStaked = re.search("(?s)(?<=currently ).*?(?= of)", title)
+    geminiStaked = geminiStaked.group(0)
+    print("Total Staked on Gemini: " + geminiStaked)
+    geminiStakedStr = geminiStaked
+    geminiStaked = geminiStaked.replace(",", "")
+    geminiStaked = float(geminiStaked)
+    
+    # # Spedn Pool
+    page = await browser.newPage()
+    await page.goto('https://app.flexa.network/explore/app/spedn')
+    time.sleep(7)
+    element = await page.querySelector(".css-i3vmyo")
+    title = await page.evaluate('(element) => element.textContent', element)
+    spednStaked = re.search("(?s)(?<=currently ).*?(?= of)", title)
+    spednStaked = spednStaked.group(0)
+    spednStakedstr = spednStaked
+    print("Total Staked on Spedn: " + spednStaked)
+    spednStaked = spednStaked.replace(",", "")
+    spednStaked = float(spednStaked)
+
+
+    # # Lightning Pool
+    page = await browser.newPage()
+    await page.goto('https://app.flexa.network/explore/transformer/lightning')
+    time.sleep(7)
+    element = await page.querySelector(".css-i3vmyo")
+    title = await page.evaluate('(element) => element.textContent', element)
+    lightningStaked = re.search("(?s)(?<=currently ).*?(?= of)", title)
+    lightningStaked = lightningStaked.group(0)
+    lightningStakedStr = lightningStaked
+    print("Total Staked on Lightning: " + lightningStaked)
+    lightningStaked = lightningStaked.replace(",", "")
+    lightningStaked = float(lightningStaked)
+
+    # Pulls the current circ supply from the website
+    page = await browser.newPage()
+    time.sleep(2)
+    await page.goto('https://amptoken.org/metrics/circulating-supply/')
+    element = await page.querySelector("pre")
+    title = await page.evaluate('(element) => element.textContent', element)
+    totalCircTokens = float(title)
+
+    
+
+    stakedPercentage = (((geminiStaked+spednStaked+lightningStaked)/totalCircTokens)*100)
+    stakedPercentage = round(stakedPercentage,2)
+    stakedPercentageStr = str(stakedPercentage)
+    print("Total Staked Percentage: " + stakedPercentageStr + "%")
+
+    
+
+
+
+
+
+
+
+    await browser.close()
+
+
+asyncio.get_event_loop().run_until_complete(main())

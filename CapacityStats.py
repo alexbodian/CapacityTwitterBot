d3efcc458fb39a7e1c60b29f929786d5d03dadf8
Alex <polarzombies@gmail.com>
2022-02-14 13:21:48 -0500

    formatted tweet and everything works; left out api creds

--- CapacityStats.py
+++ CapacityStats.py
@@ -9,72 +9,82 @@ import time
 import pyppeteer
 from pyppeteer import launch
 
+
 locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
 async def main():
     browser = await launch()
 
-    # # Gemini Pool
-    # page = await browser.newPage()
-    # await page.goto('https://app.flexa.network/explore/app/gemini')
-    # time.sleep(7)
-    # element = await page.querySelector(".css-i3vmyo")
-    # title = await page.evaluate('(element) => element.textContent', element)
-    # geminiStaked = re.search("(?s)(?<=currently ).*?(?= of)", title)
-    # geminiStaked = geminiStaked.group(0)
+    # Gemini Pool
+    page = await browser.newPage()
+    await page.goto('https://app.flexa.network/explore/app/gemini')
+    time.sleep(7)
+    element = await page.querySelector(".css-i3vmyo")
+    title = await page.evaluate('(element) => element.textContent', element)
+    geminiStaked = re.search("(?s)(?<=currently ).*?(?= of)", title)
+    geminiStaked = geminiStaked.group(0)
     # print("Total Staked on Gemini: " + geminiStaked)
-    # geminiStakedStr = geminiStaked
-    # geminiStaked = geminiStaked.replace(",", "")
-    # geminiStaked = float(geminiStaked)
+    geminiStakedStr = geminiStaked
+    geminiStaked = geminiStaked.replace(",", "")
+    geminiStaked = float(geminiStaked)
     
-    # # # Spedn Pool
-    # page = await browser.newPage()
-    # await page.goto('https://app.flexa.network/explore/app/spedn')
-    # time.sleep(7)
-    # element = await page.querySelector(".css-i3vmyo")
-    # title = await page.evaluate('(element) => element.textContent', element)
-    # spednStaked = re.search("(?s)(?<=currently ).*?(?= of)", title)
-    # spednStaked = spednStaked.group(0)
-    # spednStakedstr = spednStaked
+    # # Spedn Pool
+    page = await browser.newPage()
+    await page.goto('https://app.flexa.network/explore/app/spedn')
+    time.sleep(7)
+    element = await page.querySelector(".css-i3vmyo")
+    title = await page.evaluate('(element) => element.textContent', element)
+    spednStaked = re.search("(?s)(?<=currently ).*?(?= of)", title)
+    spednStaked = spednStaked.group(0)
+    spednStakedstr = spednStaked
     # print("Total Staked on Spedn: " + spednStaked)
-    # spednStaked = spednStaked.replace(",", "")
-    # spednStaked = float(spednStaked)
+    spednStaked = spednStaked.replace(",", "")
+    spednStaked = float(spednStaked)
 
 
-    # # # Lightning Pool
-    # page = await browser.newPage()
-    # await page.goto('https://app.flexa.network/explore/transformer/lightning')
-    # time.sleep(7)
-    # element = await page.querySelector(".css-i3vmyo")
-    # title = await page.evaluate('(element) => element.textContent', element)
-    # lightningStaked = re.search("(?s)(?<=currently ).*?(?= of)", title)
-    # lightningStaked = lightningStaked.group(0)
-    # lightningStakedStr = lightningStaked
+    # # Lightning Pool
+    page = await browser.newPage()
+    await page.goto('https://app.flexa.network/explore/transformer/lightning')
+    time.sleep(7)
+    element = await page.querySelector(".css-i3vmyo")
+    title = await page.evaluate('(element) => element.textContent', element)
+    lightningStaked = re.search("(?s)(?<=currently ).*?(?= of)", title)
+    lightningStaked = lightningStaked.group(0)
+    lightningStakedStr = lightningStaked
     # print("Total Staked on Lightning: " + lightningStaked)
-    # lightningStaked = lightningStaked.replace(",", "")
-    # lightningStaked = float(lightningStaked)
+    lightningStaked = lightningStaked.replace(",", "")
+    lightningStaked = float(lightningStaked)
 
-    # # Pulls the current circ supply from the website
-    # page = await browser.newPage()
-    # time.sleep(2)
-    # await page.goto('https://amptoken.org/metrics/circulating-supply/')
-    # element = await page.querySelector("pre")
-    # title = await page.evaluate('(element) => element.textContent', element)
-    # totalCircTokens = float(title)
-    # totalCircTokensFormatted = locale.format_string("%d", totalCircTokens, grouping=True)
+    # Pulls the current circ supply from the website
+    page = await browser.newPage()
+    time.sleep(2)
+    await page.goto('https://amptoken.org/metrics/circulating-supply/')
+    element = await page.querySelector("pre")
+    title = await page.evaluate('(element) => element.textContent', element)
+    totalCircTokens = float(title)
+    totalCircTokensFormatted = locale.format_string("%d", totalCircTokens, grouping=True)
     
     
-    # totalTokensStaked = geminiStaked+spednStaked+lightningStaked
-    # totalTokensStakedFormatted = locale.format_string("%d", totalTokensStaked, grouping=True)
+    totalTokensStaked = geminiStaked+spednStaked+lightningStaked
+    totalTokensStakedFormatted = locale.format_string("%d", totalTokensStaked, grouping=True)
 
 
-    # stakedPercentage = (((geminiStaked+spednStaked+lightningStaked)/totalCircTokens)*100)
-    # stakedPercentage = round(stakedPercentage,2)
-    # stakedPercentageStr = str(stakedPercentage)
+    stakedPercentage = (((geminiStaked+spednStaked+lightningStaked)/totalCircTokens)*100)
+    stakedPercentage = round(stakedPercentage,2)
+    stakedPercentageStr = str(stakedPercentage)
     # print("Total number of tokens staked out of circulating supply: " + totalTokensStakedFormatted + " / " + totalCircTokensFormatted)
     # print("Total Staked Percentage: " + stakedPercentageStr + "%")
 
+    
+    tweet1 = "Flexa Capacity Stats\n"
+    tweet2 = "Total Staked on Gemini: " + geminiStakedStr  
+    tweet3 = "\nTotal Staked on Spedn: " + spednStakedstr 
+    tweet4 = "\nTotal Staked on Lightning: " + lightningStakedStr
+    tweet5 = "\nTotal number of tokens staked out of circulating supply: " + totalTokensStakedFormatted + " / " + totalCircTokensFormatted 
+    tweet6 = "\nTotal Staked Percentage: " + stakedPercentageStr + "%"
 
+    tweet = (tweet1 + tweet2 + tweet3 + tweet4 + tweet5 + tweet6)
 
+    
     authenticator = tweepy.OAuthHandler(api_key, api_key_secret)
     authenticator.set_access_token(access_token, access_token_secret)
 

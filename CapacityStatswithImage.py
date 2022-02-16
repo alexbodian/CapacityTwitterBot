fc2c1748d671749bc709eed08a8bc470c22546e3
Alex <polarzombies@gmail.com>
2022-02-16 15:44:10 -0500

    some comments in image script

--- CapacityStatswithImage.py
+++ CapacityStatswithImage.py
@@ -5,10 +5,12 @@ import re
 import time
 import locale
 import asyncio
+from PIL import Image, ImageDraw, ImageFont
 import time
 import pyppeteer
 from pyppeteer import launch
-
+# need to put starting location same as the directory of the file
+# if running with the Windows task scheduler
 
 locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
 async def main():
@@ -54,6 +56,21 @@ async def main():
     lightningStaked = lightningStaked.replace(",", "")
     lightningStaked = float(lightningStaked)
 
+    # APY Percentages
+    page = await browser.newPage()
+    await page.goto('https://app.flexa.network/account/supply')
+    time.sleep(7)
+    element = await page.querySelectorAll(".css-1bvmc47")
+    geminiAPY = await page.evaluate('(element) => element.textContent', element[0])
+    spednAPY = await page.evaluate('(element) => element.textContent', element[1])
+    lightningAPY = await page.evaluate('(element) => element.textContent', element[2])
+    
+    geminiAPY = geminiAPY.replace("APY", "")
+    spednAPY = spednAPY.replace("APY", "")
+    lightningAPY =lightningAPY.replace("APY", "")
+
+
+
     # Pulls the current circ supply from the website
     page = await browser.newPage()
     time.sleep(2)
@@ -75,22 +92,45 @@ async def main():
     # print("Total Staked Percentage: " + stakedPercentageStr + "%")
 
     
-    tweet1 = "Flexa Capacity Stats\n\n"
-    tweet2 = "Staked on Gemini: " + geminiStakedStr  + " ₳"
-    tweet3 = "\nStaked on Spedn: " + spednStakedstr + " ₳"
-    tweet4 = "\nStaked on Lightning: " + lightningStakedStr + " ₳"
-    tweet5 = "\nTokens staked out of the circulating supply: \n" + totalTokensStakedFormatted + " ₳ / " + totalCircTokensFormatted + " ₳"
-    tweet6 = "\nTotal Staked Percentage: " + stakedPercentageStr + "%\n" + "$AMP #flexa #amp"
+    tweet1 = "                              Flexa Capacity Stats\n\n"
+    tweet2 = "Staked on Gemini: " + geminiStakedStr  + " ₳\n" + "APY on Gemini: "  + geminiAPY
+    tweet3 = "\n\nStaked on Spedn: " + spednStakedstr + " ₳\n" + "APY on Spedn: "  + spednAPY
+    tweet4 = "\n\nStaked on Lightning: " + lightningStakedStr + " ₳\n" + "APY on Lightning: "  + lightningAPY
+    tweet5 = "Tokens staked out of the circulating supply: \n" + totalTokensStakedFormatted + " ₳ / " + totalCircTokensFormatted + " ₳"
+    tweet6 = "\n\nTotal Staked Percentage: " + stakedPercentageStr + "%\n"+ "$AMP #flexa #amp"
 
-    tweet = (tweet1 + tweet2 + tweet3 + tweet4 + tweet5 + tweet6)
-   
+    tweet = (tweet1 + tweet2 + tweet3 + tweet4)
+    tweetText = (tweet5 + tweet6)
+
+
+    # https://python.plainenglish.io/generating-text-on-image-with-python-eefe4430fe77
+    # https://pillow.readthedocs.io/en/stable/reference/ImageDraw.html
+    width = 512
+    height = 305
+    img = Image.new('RGB', (width, height), color='black')
+    fnt = ImageFont.truetype("Noto_Sans/NotoSans-Regular.ttf", 20)
+    imgDraw = ImageDraw.Draw(img)
+
+    imgDraw.text((10, 10), tweet,font=fnt, fill=(255, 255, 255))
+
+    currentTime = time.time()
+    currentTime = int(currentTime)
+    currentTime = str(currentTime)
+
+    img.save('{}.png'.format(currentTime))
+
+    time.sleep(3)
+
     authenticator = tweepy.OAuthHandler(api_key, api_key_secret)
     authenticator.set_access_token(access_token, access_token_secret)
 
     api = tweepy.API(authenticator, wait_on_rate_limit=True)
 
-    # print(tweet)
-    api.update_status(tweet)
+    # upload image
+    media = api.media_upload('{}.png'.format(currentTime))
+
+    # post tweet with image
+    api.update_status(status=tweetText,media_ids=[media.media_id])
 
 
 

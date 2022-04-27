b414728fc2bb50ac52f618812c31320f9be81474
Alex <polarzombies@gmail.com>
2022-04-27 15:50:26 -0400

    implementing new pools

--- CapacityStatswithImage.py
+++ CapacityStatswithImage.py
@@ -11,10 +11,6 @@ from PIL import Image, ImageDraw, ImageFont
 import time
 import pyppeteer
 from pyppeteer import launch
-
-
-
-
 # need to put starting location same as the directory of the file
 # if running with the Windows task scheduler
 
@@ -22,43 +18,52 @@ locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
 async def main():
     browser = await launch()
 
-    dateCurr = date.today()
-    dateCurr = dateCurr.strftime("%m/%d/%y")
 
-    timeCurr = datetime.now()
-    timeCurr = timeCurr.strftime("%H:%M")
-
-    # Gemini Pool
-    page = await browser.newPage()
-    await page.goto('https://app.flexa.network/explore/app/gemini')
-    time.sleep(7)
-    element = await page.querySelector(".css-i3vmyo")
-    title = await page.evaluate('(element) => element.textContent', element)
-    geminiStaked = re.search("(?s)(?<=currently ).*?(?= of)", title)
-    geminiStaked = geminiStaked.group(0)
-    # print("Total Staked on Gemini: " + geminiStaked)
-    geminiStakedStr = geminiStaked
-    geminiStaked = geminiStaked.replace(",", "")
-    geminiStaked = float(geminiStaked)
     
-    # # Spedn Pool
-    page = await browser.newPage()
-    await page.goto('https://app.flexa.network/explore/app/spedn')
-    time.sleep(7)
-    element = await page.querySelector(".css-i3vmyo")
-    title = await page.evaluate('(element) => element.textContent', element)
-    spednStaked = re.search("(?s)(?<=currently ).*?(?= of)", title)
-    spednStaked = spednStaked.group(0)
-    spednStakedstr = spednStaked
-    # print("Total Staked on Spedn: " + spednStaked)
-    spednStaked = spednStaked.replace(",", "")
-    spednStaked = float(spednStaked)
+    # # Gemini Pool
+    # page = await browser.newPage()
+    # await page.goto('https://app.flexa.network/explore/app/gemini')
+    # time.sleep(10)
+    # element = await page.querySelector(".css-i3vmyo")
+    # title = await page.evaluate('(element) => element.textContent', element)
+    # geminiStaked = re.search("(?s)(?<=currently ).*?(?= of)", title)
+    # geminiStaked = geminiStaked.group(0)
+    # # print("Total Staked on Gemini: " + geminiStaked)
+    # geminiStakedStr = geminiStaked
+    # geminiStaked = geminiStaked.replace(",", "")
+    # geminiStaked = float(geminiStaked)
+    
+    # # # Spedn Pool
+    # page = await browser.newPage()
+    # await page.goto('https://app.flexa.network/explore/app/spedn')
+    # time.sleep(11)
+    # element = await page.querySelector(".css-i3vmyo")
+    # title = await page.evaluate('(element) => element.textContent', element)
+    # spednStaked = re.search("(?s)(?<=currently ).*?(?= of)", title)
+    # spednStaked = spednStaked.group(0)
+    # spednStakedstr = spednStaked
+    # # print("Total Staked on Spedn: " + spednStaked)
+    # spednStaked = spednStaked.replace(",", "")
+    # spednStaked = float(spednStaked)
 
 
-    # # Lightning Pool
+    # # # Lightning Pool
+    # page = await browser.newPage()
+    # await page.goto('https://app.flexa.network/explore/transformer/lightning')
+    # time.sleep(11)
+    # element = await page.querySelector(".css-i3vmyo")
+    # title = await page.evaluate('(element) => element.textContent', element)
+    # lightningStaked = re.search("(?s)(?<=currently ).*?(?= of)", title)
+    # lightningStaked = lightningStaked.group(0)
+    # lightningStakedStr = lightningStaked
+    # # print("Total Staked on Lightning: " + lightningStaked)
+    # lightningStaked = lightningStaked.replace(",", "")
+    # lightningStaked = float(lightningStaked)
+
+    # # Bitcoin Pool
     page = await browser.newPage()
-    await page.goto('https://app.flexa.network/explore/transformer/lightning')
-    time.sleep(7)
+    await page.goto('https://app.flexa.network/explore/transformer/bitcoin')
+    time.sleep(11)
     element = await page.querySelector(".css-i3vmyo")
     title = await page.evaluate('(element) => element.textContent', element)
     lightningStaked = re.search("(?s)(?<=currently ).*?(?= of)", title)
@@ -71,7 +76,7 @@ async def main():
     # APY Percentages
     page = await browser.newPage()
     await page.goto('https://app.flexa.network/account/supply')
-    time.sleep(7)
+    time.sleep(11)
     element = await page.querySelectorAll(".css-1bvmc47")
     geminiAPY = await page.evaluate('(element) => element.textContent', element[0])
     spednAPY = await page.evaluate('(element) => element.textContent', element[1])
@@ -85,7 +90,7 @@ async def main():
 
     # Pulls the current circ supply from the website
     page = await browser.newPage()
-    time.sleep(2)
+    time.sleep(11)
     await page.goto('https://amptoken.org/metrics/circulating-supply/')
     element = await page.querySelector("pre")
     title = await page.evaluate('(element) => element.textContent', element)
@@ -114,6 +119,9 @@ async def main():
     current = totalTokensStaked
     difference = current  - int(float(lastStakedValue)) 
     
+
+
+
     # negative
     if difference < 0:
         difference = difference * -1 
@@ -156,28 +164,12 @@ async def main():
 
     img.save('./image/{}.png'.format(currentTime))
 
-    time.sleep(3)
-
-
-    # tweet1 = "                              Flexa Capacity Stats\n\n"
-    # tweet2 =   "    Pool                 APY                Amount of AMP Staked                       \n"
-    # tweet2_5 = " -----------         ----------          ----------------------------------                 \n"
-    # tweet2_75 = "   Spedn             " + spednAPY + "                 "  +spednStakedstr +  " ₳\n\n" 
-    # tweet3 = "  Gemini            " + geminiAPY + "                 "  +geminiStakedStr +  " ₳\n\n" 
-    # tweet4 = " Lightning         " + lightningAPY + "                  "  +lightningStakedStr +  " ₳\n\n" 
-    # tweet5 = "Tokens staked out of the circulating supply: \n" + totalTokensStakedFormatted + " ₳ / " + totalCircTokensFormatted + " ₳"
-    # tweet6 = "\n\nTotal Staked Percentage: " + stakedPercentageStr + "%\n"+ "$AMP #flexa #amp"
+    time.sleep(6)
 
 
 
 
-    # Date,Time,Spedn,Gemini,Lightning,SpednAPY,GeminiAPY,LightningAPY
-    with open("./capacityStats.csv", "a", newline='') as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow([dateCurr, timeCurr, spednStakedstr,geminiStakedStr,lightningStakedStr,spednAPY,geminiAPY,lightningAPY])
 
-
-    exit()
 
     authenticator = tweepy.OAuthHandler(api_key, api_key_secret)
     authenticator.set_access_token(access_token, access_token_secret)
@@ -190,6 +182,16 @@ async def main():
     # post tweet with image
     api.update_status(status=tweetText,media_ids=[media.media_id])
 
+    dateCurr = date.today()
+    dateCurr = dateCurr.strftime("%m/%d/%y")
+
+    timeCurr = datetime.now()
+    timeCurr = timeCurr.strftime("%H:%M")
+    
+        # Date,Time,Spedn,Gemini,Lightning,SpednAPY,GeminiAPY,LightningAPY
+    with open("./capacityStats.csv", "a", newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow([dateCurr, timeCurr, spednStakedstr,geminiStakedStr,lightningStakedStr,spednAPY,geminiAPY,lightningAPY, ""])
 
     await page.close()
     await browser.close()
